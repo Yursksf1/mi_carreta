d14dc8ac18d21200c42ddd203691639ca37400fd
yurley <yurs.ksf1@gmail.com>
2023-03-25 12:45:08 -0500

    espannol

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,6 +129,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DECIMAL_SEPARATOR = '.'
 
 STATIC_URL = '/static/'
 
